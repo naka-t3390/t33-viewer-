508e596a959f9c601e179e54703c3f4975a24d9d
naka-t <nakamura-tsuyoshi12@aeonpeople.biz>
2026-06-20 19:24:59 +0900

    config: OAuthクライアントIDを設定

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // 公開前提の設定のみ（秘密情報を入れない）。
 // CLIENT_ID は Google Cloud で発行した「ウェブアプリ」OAuthクライアントID。
 export const CONFIG = {
-  CLIENT_ID: "REPLACE_WITH_YOUR_OAUTH_CLIENT_ID.apps.googleusercontent.com",
+  CLIENT_ID: "206549851901-1mamv0fdh4n09ab45fa0e8efvkafeush.apps.googleusercontent.com",
   SCOPE: "https://www.googleapis.com/auth/drive.readonly",
   ROOT_FOLDER: "T33Monitor",
 };
